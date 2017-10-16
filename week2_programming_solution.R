7ea5c966d4eabb82355f81ea7796f5e0e6cad348
Taeyoung Ahn <taeyoung.a@gmail.com>
2017-10-16 21:51:16 +0900

    pollutantmean solution edited

--- week2_programming_solution.R
+++ week2_programming_solution.R
@@ -1,7 +1,8 @@
 rm(list=ls()) #Clear memory
 
-setwd("D:/Coursera") #Set Workind directory
+setwd("D:/Coursera") #Set Working directory
 
+##PART1##
 pollutantmean <- function(directory, pollutant, id=1:332){ #As given by the question
         pol_vect <- c() #Create an empty vector to store pollutant data
         all_files <- as.character(list.files(directory)) #Create list of all files
@@ -21,3 +22,56 @@ pollutantmean <- function(directory, pollutant, id=1:332){ #As given by the ques
 # > pollutantmean("specdata","nitrate",23)
 # [1] 1.280833 - CORRECT
 #############################################
+
+##PART2##
+complete <- function(directory, id=1:332){
+        all_files <- as.character(list.files(directory)) #Create list of all files
+        file_paths <- paste(directory,all_files,sep="/") #Create a list of files with full path (directory/filename)
+        #result <- data.frame(id = integer(), nobs = integer())
+        datalist <- list()
+        j <- 1
+        for(i in id){
+                comp <- read.csv(file_paths[i])
+                comp_row <- nrow(comp[complete.cases(comp), ])
+                datalist[[j]] <- comp_row
+                j <- j+1
+        }
+        #result = do.call(rbind, datalist)
+        result <- as.data.frame(cbind(id,nobs = datalist))
+        return(result)
+}
+
+complete("specdata",2:3)
+
+
+corr <- function(directory, threshold = 0) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'threshold' is a numeric vector of length 1 indicating the
+        ## number of completely observed observations (on all
+        ## variables) required to compute the correlation between
+        ## nitrate and sulfate; the default is 0
+        
+        ## Return a numeric vector of correlations
+        
+        # get the complete table
+        complete_table <- complete("specdata", 1:332)
+        nobs <- complete_table$nobs
+        # find the valid ids
+        ids <- complete_table$id[nobs > threshold]
+        # get the length of ids vector
+        id_len <- length(ids)
+        corr_vector <- rep(0, id_len)
+        # find all files in the specdata folder
+        all_files <- as.character( list.files(directory) )
+        file_paths <- paste(directory, all_files, sep="/")
+        j <- 1
+        for(i in ids) {
+                current_file <- read.csv(file_paths[i], header=T, sep=",")
+                corr_vector[j] <- cor(current_file$sulfate, current_file$nitrate, use="complete.obs")
+                j <- j + 1
+        }
+        result <- corr_vector
+        return(result)   
+}
